f472320640501b331ace6cd5f68c88f0ef3dd23b
Dev-jwJeong <rgn95652@gmail.com>
2023-01-18 19:31:57 +0900

    feat : layout 하위에 route 적용

--- src/components/common/Router.tsx
+++ src/components/common/Router.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import { Home } from '../../pages';
+import { Layout } from '../Layout';
 
 const Router = () => (
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+      </Route>
     </Routes>
   </BrowserRouter>
 );
