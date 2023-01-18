f329f5cb2e91e7c8976c805f37dbc05cd4c9c864
Dev-jwJeong <rgn95652@gmail.com>
2023-01-19 01:11:12 +0900

    style : 회원가입 페이지

---
+++ src/pages/Register.tsx
@@ -0,0 +1,10 @@
+import { RegisterForm } from '../components/Auth';
+import { MainLayout } from '../components/Layout';
+
+const Register = () => (
+  <MainLayout>
+    <RegisterForm />
+  </MainLayout>
+);
+
+export default Register;
