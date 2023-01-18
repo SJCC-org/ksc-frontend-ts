543c1d91d7684999a9b694b428ba5c5de3e110b3
Dev-jwJeong <rgn95652@gmail.com>
2023-01-18 19:30:01 +0900

    style : ThemeProvider 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import { ThemeProvider } from 'styled-components';
 
 import Router from './components/common/Router';
 import { GlobalStyle } from './styles/globalStyle';
+import theme from './styles/theme';
 
 const App = () => (
   <div>
-    <GlobalStyle />
-    <Router />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Router />
+    </ThemeProvider>
   </div>
 );
 
