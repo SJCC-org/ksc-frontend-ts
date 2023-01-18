827a1db3680ae219c95b649f94cab1bd57678db0
Dev-jwJeong <rgn95652@gmail.com>
2023-01-19 01:12:01 +0900

    feat : 회원가입 route

--- src/components/common/Router.tsx
+++ src/components/common/Router.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import { Home } from '../../pages';
+import { Home, Register } from '../../pages';
 import { Layout } from '../Layout';
 
 const Router = () => (
@@ -8,6 +8,7 @@ const Router = () => (
     <Routes>
       <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
       </Route>
     </Routes>
   </BrowserRouter>
