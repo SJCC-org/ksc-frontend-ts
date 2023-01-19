adb6848450784db9ca6ed49160dff45d2ff9d529
Dev-jwJeong <rgn95652@gmail.com>
2023-01-19 23:07:24 +0900

    feat : 회원가입 기능 추가

--- src/components/Auth/RegisterForm.tsx
+++ src/components/Auth/RegisterForm.tsx
@@ -1,5 +1,9 @@
-import { SubmitHandler, useForm } from 'react-hook-form';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+
+import { getDuplicateUser, postEmailAuthentication, postEmailCode, postSignUp } from '../../lib/api/auth';
 
 interface RegisterInfo {
   name: string;
@@ -11,14 +15,42 @@ interface RegisterInfo {
 }
 
 const RegisterForm = () => {
-  const { register, handleSubmit } = useForm<RegisterInfo>();
+  const [openEmail, setOpenEmail] = useState(false);
+  const [codeMessage, setCodeMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [checkPassword, setCheckPassword] = useState(false);
 
-  const handleSubmitRegister: SubmitHandler<RegisterInfo> = (data) => {
-    console.log(data);
+  const { register, watch, handleSubmit } = useForm<RegisterInfo>();
+
+  const navigate = useNavigate();
+
+  const handleDuplicateUser = async () => {
+    const res = await getDuplicateUser(watch('username'));
+    res?.data ? setErrorMessage('아이디가 중복됩니다.') : setErrorMessage('사용가능한 아이디 입니다.');
   };
+
+  const handleCheckPassword = () => {
+    if (watch('password') !== watch('passwordConfirm')) setCheckPassword(!checkPassword);
+  };
+
+  const handleEmailAuthentication = async () => {
+    const res = await postEmailAuthentication(watch('email'));
+    res?.status && setOpenEmail(!openEmail);
+  };
+
+  const handleCheckCode = async () => {
+    const res = await postEmailCode(watch('email'), watch('code'));
+    res?.data ? setCodeMessage('인증코드가 일치합니다.') : setCodeMessage('인증코드가 일치하지 않습니다.');
+  };
+
+  const handleSignUp = async () => {
+    const res = await postSignUp(watch('username'), watch('password'), watch('name'), watch('email'));
+    res?.data && navigate('/');
+  };
+
   return (
     <RegisterFormWrapper>
-      <RegisterFormBlock onSubmit={handleSubmit(handleSubmitRegister)}>
+      <RegisterFormBlock onSubmit={handleSubmit(handleSignUp)}>
         <InputBlock>
           <InputLabelText>이름</InputLabelText>
           <StyledInput type="text" placeholder="이름을 입력해주세요" {...register('name', { required: true })} />
@@ -31,9 +63,12 @@ const RegisterForm = () => {
               placeholder="아이디를 입력해주세요"
               {...register('username', { required: true })}
             />
-            <DuplivateBtn type="button">중복확인</DuplivateBtn>
+            <DuplicateBtn type="button" onClick={handleDuplicateUser}>
+              중복확인
+            </DuplicateBtn>
           </DuplicateBlock>
         </InputBlock>
+        <ErrorMessageBlock>{errorMessage}</ErrorMessageBlock>
         <InputBlock>
           <InputLabelText>비밀번호</InputLabelText>
           <StyledInput
@@ -47,16 +82,31 @@ const RegisterForm = () => {
           <StyledInput
             type="password"
             placeholder="비밀번호를 한번 더 입력해주세요"
+            onKeyUp={handleCheckPassword}
             {...register('passwordConfirm', { required: true })}
           />
         </InputBlock>
+        {checkPassword && <ErrorMessageBlock>비밀번호가 일치하지 않습니다.</ErrorMessageBlock>}
         <InputBlock>
           <InputLabelText>이메일</InputLabelText>
           <DuplicateBlock>
             <StyledInput type="email" placeholder="이메일을 입력해주세요" {...register('email', { required: true })} />
-            <DuplivateBtn type="button">확인</DuplivateBtn>
+            <DuplicateBtn type="button" onClick={handleEmailAuthentication}>
+              인증하기
+            </DuplicateBtn>
           </DuplicateBlock>
         </InputBlock>
+        {openEmail && (
+          <InputBlock>
+            <DuplicateBlock>
+              <StyledInput type="text" placeholder="인증코드를 입력해주세요" {...register('code')} />
+              <DuplicateBtn type="button" onClick={handleCheckCode}>
+                확인
+              </DuplicateBtn>
+            </DuplicateBlock>
+          </InputBlock>
+        )}
+        <ErrorMessageBlock>{codeMessage}</ErrorMessageBlock>
         <SubmitBtn type="submit">회원가입</SubmitBtn>
       </RegisterFormBlock>
     </RegisterFormWrapper>
@@ -95,7 +145,7 @@ const DuplicateBlock = styled.div`
     flex-direction: column;
   }
 `;
-const DuplivateBtn = styled.button`
+const DuplicateBtn = styled.button`
   width: 20%;
   margin-left: 1rem;
 
@@ -145,4 +195,9 @@ const StyledInput = styled.input`
     border: 2px solid ${({ theme }) => theme.colors.Kakao_Color_Yellow};
   }
 `;
-const ErrorMessageBlock = styled.div``;
+const ErrorMessageBlock = styled.div`
+  color: red;
+  font-weight: bold;
+
+  text-align: center;
+`;
